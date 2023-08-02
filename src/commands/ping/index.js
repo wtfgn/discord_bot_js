183d6bb54408fee5141a6753db5b04e7e7d32508
wtfgn <exal59@yahoo.com>
2023-08-02 23:08:25 +0800

    User & Server slash commands

--- src/commands/ping/index.js
+++ src/commands/ping/index.js
@@ -1,9 +1,9 @@
 import { SlashCommandBuilder } from 'discord.js';
 
-export const command = new SlashCommandBuilder()
+export const data = new SlashCommandBuilder()
 	.setName('ping')
 	.setDescription('Replies with Pong!');
 
 export const execute = async (interaction) => {
-	await interaction.reply(`Pong! ${interaction.client.ws.ping}ms, This command is run by ${interaction.user.username}`);
+	await interaction.reply(`Pong! ${interaction.client.ws.ping}ms`);
 };
