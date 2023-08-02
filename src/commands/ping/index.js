dd1f9e9badf6955164d85c50def0d004e089fc77
wtfgn <exal59@yahoo.com>
2023-08-02 22:56:26 +0800

    Modify reply action

--- src/commands/ping/index.js
+++ src/commands/ping/index.js
@@ -5,5 +5,5 @@ export const command = new SlashCommandBuilder()
 	.setDescription('Replies with Pong!');
 
 export const execute = async (interaction) => {
-	await interaction.reply('Pong!');
+	await interaction.reply(`Pong! ${interaction.client.ws.ping}ms, This command is run by ${interaction.user.username}`);
 };
