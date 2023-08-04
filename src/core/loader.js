0b0289d7a81e2dd116b1ea7ab1f250e4a9aca15a
wtfgn <exal59@yahoo.com>
2023-08-04 14:04:09 +0800

    Refinement of the loaders

--- src/core/loader.js
+++ src/core/loader.js
@@ -4,29 +4,42 @@ import { useAppStore } from '@/store/app.js';
 
 const updateSlashCommands = async (guildID, commandData) => {
 	const rest = new REST({ version: '10' }).setToken(process.env.BOT_TOKEN);
-	await rest.put(
-		Routes.applicationGuildCommands(
-			process.env.APP_ID,
-			guildID,
-		),
-		{ body: commandData },
-	);
+	try {
+		await rest.put(
+			Routes.applicationGuildCommands(process.env.CLIENT_ID, guildID),
+			{ body: commandData },
+		);
+		console.log(`Successfully registered ${commandData.length} application commands.`);
+	}
+	catch (error) {
+		console.error(error);
+	}
 };
 
 export const loadCommands = async () => {
 	const appStore = useAppStore();
+	const client = appStore.client;
 	const commandData = [];
 	const commands = new Collection();
 	const files = await fg('./src/commands/**/index.js');
 	for (const file of files) {
+		// Import command
 		const command = await import(file);
-		// Push command data to array
-		commandData.push(command.data);
-		// Add command to collection
-		commands.set(command.data.name, command.execute);
+		// Check if command has data and execute properties
+		if ('data' in command && 'execute' in command) {
+			// Push command data to array
+			commandData.push(command.data.toJSON());
+			// Add command to collection
+			commands.set(command.data.name, command);
+		}
+		else {
+			console.error(`Command ${file} is missing data or execute property`);
+		}
 	}
 	// Set commands collection to app store
 	appStore.commandsActionMap = commands;
+	// Updata client commands
+	client.commands = commands;
 	// Update slash commands
 	updateSlashCommands(process.env.GUILD_ID, commandData);
 };
--- src/events/interaction_create/index.js
+++ src/events/interaction_create/index.js
@@ -3,21 +3,41 @@ import { useAppStore } from '@/store/app.js';
 
 export const data = {
 	name: Events.InteractionCreate,
-	once: false,
 };
 
 export const execute = async interaction => {
 	const appStore = useAppStore();
 	const commandsActionMap = appStore.commandsActionMap;
-	if (!interaction.isCommand()) return;
-	if (!commandsActionMap.has(interaction.commandName)) return;
+	// Get command
+	const command = commandsActionMap.get(interaction.commandName);
+
+	// Check if it is a chat input command
+	if (!interaction.isChatInputCommand()) return;
+	// Check if command exists
+	if (!command) {
+		console.error(`Command ${interaction.commandName} does not exist`);
+		return;
+	}
+
 	try {
 		// Execute command
-		await commandsActionMap.get(interaction.commandName)(interaction);
+		await command.execute(interaction);
 	}
 	catch (error) {
 		console.error(error);
-		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		// If interaction is already replied or deferred
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({
+				content: 'There was an error while executing this command!',
+				ephemeral: true,
+			});
+		}
+		else {
+			await interaction.reply({
+				content: 'There was an error while executing this command!',
+				ephemeral: true,
+			});
+		}
 	}
 };
 
