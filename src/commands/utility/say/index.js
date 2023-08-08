49556405eedfb6d5baf805f37e5f24a25701cedb
wtfgn <exal59@yahoo.com>
2023-08-08 16:21:55 +0800

    Add say command

---
+++ src/commands/utility/say/index.js
@@ -0,0 +1,25 @@
+import { SlashCommandBuilder } from 'discord.js';
+import wait from 'node:timers/promises';
+
+export const data = new SlashCommandBuilder()
+	.setName('say')
+	.setDescription('Replies with your input!')
+	.addStringOption(option =>
+		option.setName('input')
+			.setDescription('The input to echo back')
+			.setRequired(true));
+
+export const execute = async (interaction) => {
+	const { channel } = interaction;
+	const input = interaction.options.getString('input');
+
+	await interaction.reply({ content: 'Message sent!', ephemeral: true });
+
+	// Send message
+	await channel.sendTyping();
+	await channel.send(input);
+
+	// Reply to interaction
+	await wait.setTimeout(3000);
+	await interaction.deleteReply();
+};
