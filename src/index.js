177dd07da88f881c581942d50c758b057996f3d7
wtfgn <exal59@yahoo.com>
2023-08-17 12:06:08 +0800

    Change to use other package

--- src/index.js
+++ src/index.js
@@ -3,11 +3,9 @@ import vueInit from '@/core/vue.js';
 import { Client, GatewayIntentBits, Partials } from 'discord.js';
 import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
-import { loadCommands, loadEvents } from '@/core/loader.js';
+import { loadCommands, loadEvents, loadPlayerEvents, deleteGuildCommands, deleteGlobalCommands } from '@/utils/loader.js';
 import { useAppStore } from '@/store/app.js';
-import { DisTube } from 'distube';
-import { SpotifyPlugin } from '@distube/spotify';
-import { YtDlpPlugin } from '@distube/yt-dlp';
+import { createPlayer } from '@/core/discord_player/create_player.js';
 
 // Initialize Vue.js
 vueInit();
@@ -43,59 +41,24 @@ const client = new Client({
 		Partials.Reaction,
 	],
 });
-
+createPlayer(client).then(player => {
+	// Add event listeners to discord player
+	loadPlayerEvents(player);
+});
 // Initialize app store
 const appStore = useAppStore();
 
-// Initialize Distube
-const distube = new DisTube(client, {
-	emitNewSongOnly: true,
-	leaveOnFinish: true,
-	emitAddSongWhenCreatingQueue: false,
-	emitAddListWhenCreatingQueue: false,
-	plugins: [
-		new SpotifyPlugin(),
-		new YtDlpPlugin({ update: false }),
-	],
-});
-
-const status = queue =>
-	`Volume: \`${queue.volume}%\` | Filter: \`${queue.filters.names.join(', ') || 'Off'}\` | Loop: \`${queue.repeatMode ? (queue.repeatMode === 2 ? 'All Queue' : 'This Song') : 'Off'
-	}\` | Autoplay: \`${queue.autoplay ? 'On' : 'Off'}\``;
-
-distube
-	.on('playSong', (queue, song) =>
-		queue.textChannel.send(
-			`${client.emotes.play} | Playing \`${song.name}\` - \`${song.formattedDuration}\`\nRequested by: ${song.user
-			}\n${status(queue)}`,
-		),
-	)
-	.on('addSong', (queue, song) =>
-		queue.textChannel.send(
-			`${client.emotes.success} | Added ${song.name} - \`${song.formattedDuration}\` to the queue by ${song.user}`,
-		),
-	)
-	.on('addList', (queue, playlist) =>
-		queue.textChannel.send(
-			`${client.emotes.success} | Added \`${playlist.name}\` playlist (${playlist.songs.length
-			} songs) to queue\n${status(queue)}`,
-		),
-	)
-	.on('error', (channel, e) => {
-		if (channel) channel.send(`${client.emotes.error} | An error encountered: ${e.toString().slice(0, 1974)}`);
-		else console.error(e);
-	})
-	.on('empty', channel => channel.send('Voice channel is empty! Leaving the channel...'))
-	.on('searchNoResult', (message, query) =>
-		message.channel.send(`${client.emotes.error} | No result found for \`${query}\`!`),
-	)
-	.on('finish', queue => queue.textChannel.send('Finished!'));
-
 appStore.client = client;
-appStore.distube = distube;
 
 // Log in to Discord with your client's token
 client.login(process.env.BOT_TOKEN);
 
-loadCommands();
-loadEvents();
+(async () => {
+	// Delete all commands
+	await deleteGuildCommands();
+	await deleteGlobalCommands();
+
+	// Load commands and events
+	await loadCommands();
+	await loadEvents();
+})();
